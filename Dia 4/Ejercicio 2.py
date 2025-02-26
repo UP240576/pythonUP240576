2174ca3ae6706cc0efc91a9e529e37a4f39ad076
UP240576 <UP240576@alumnos.upa.edu.mx>
2025-02-25 22:23:53 -0600

    Ejercicio 1-8 dia 4

--- Dia 4/Ejercicio 2.py
+++ Dia 4/Ejercicio 2.py
@@ -1,1 +1,91 @@
-## Ejercicio 
+
+##1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+thirty = "Thirty"
+days = "Days"
+of = "Of"
+py = "Python"
+space = " "
+Tdop = thirty + space + days + space + of + space + py
+print(Tdop)
+
+##2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+
+cdng = "Coding"
+F = "For"
+All = "All"
+space = " "
+CDA = cdng + space + F + space + All
+print(CDA)
+
+##3. Declare a variable named company and assign it to an initial value "Coding For All".
+
+company = "Coding For All"
+
+##4. Print the variable company using _print()_.
+
+print(company)
+
+##5. Print the length of the company string using _len()_ method and _print()_.
+
+length_company = len(company)
+print(length_company)
+
+##6. Change all the characters to uppercase letters using _upper()_ method.
+
+print(company.upper())
+
+##7. Change all the characters to lowercase letters using _lower()_ method.
+
+print(company.lower())
+
+##8. Use capitalize(), title(), swapcase() methods to format the value of the string _Coding For All_.
+
+
+
+##9. Cut(slice) out the first word of _Coding For All_ string.
+
+
+
+##10. Check if _Coding For All_ string contains a word Coding using the method index, find or other methods.
+
+
+
+##11. Replace the word coding in the string 'Coding For All' to Python.
+
+
+
+##12. Change Python for Everyone to Python for All using the replace method or other methods.
+
+
+
+##13. Split the string 'Coding For All' using space as the separator (split()) .
+
+
+
+##14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+
+
+
+##15. What is the character at index 0 in the string _Coding For All_.
+
+
+
+##16. What is the last index of the string _Coding For All_.
+
+
+
+##17. What character is at index 10 in "Coding For All" string.
+
+
+
+##18. Create an acronym or an abbreviation for the name 'Python For Everyone'.
+
+
+
+##19. Create an acronym or an abbreviation for the name 'Coding For All'.
+
+
+
+##20. Use index to determine the position of the first occurrence of C in Coding For All.
+
