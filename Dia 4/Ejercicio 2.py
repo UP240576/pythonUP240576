3b404e25e7c3e7ceda222d3c33b37e5221f95c32
Leonardo <up240576@alumnos.upa.edu.mx>
2025-02-27 09:04:57 -0600

    Ejercicio 22 - 23

--- Dia 4/Ejercicio 2.py
+++ Dia 4/Ejercicio 2.py
@@ -114,12 +114,14 @@ print(company.index(Fo))
 
 ##22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
 
-
+Cdoa = "Coding For All People"
+print(Cdoa.rfind("l"))
 
 ##23. Use index or find to find the position of the first occurrence of the word 'because' 
 # in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
 
-
+sentence = "You cannot end a sentence with because because because is a conjunction"
+print(sentence.find("because"))
 
 ##24. Use rindex to find the position of the last occurrence of the word because in the following sentence: 
 # 'You cannot end a sentence with because because because is a conjunction'
