f856b7e90239383e5c3e2bed71028a11e0beebad
Leonardo <up240576@alumnos.upa.edu.mx>
2025-02-27 11:53:44 -0600

    Ejercicio 24 - 26

--- Dia 4/Ejercicio 2.py
+++ Dia 4/Ejercicio 2.py
@@ -114,6 +114,7 @@ print(company.index(Fo))
 
 ##22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
 
+print("22. Use rfind to determine the position of the last occurrence of l in Coding For All People.")
 Cdoa = "Coding For All People"
 print(Cdoa.rfind("l"))
 
@@ -125,5 +126,85 @@ print(sentence.find("because"))
 
 ##24. Use rindex to find the position of the last occurrence of the word because in the following sentence: 
 # 'You cannot end a sentence with because because because is a conjunction'
+
+print(sentence.rindex("because"))
+
+##25. Slice out the phrase 'because because because' in the following sentence: 
+# 'You cannot end a sentence with because because because is a conjunction'
+
+beforeBecause = sentence[0:29]
+lastBecause = sentence[54:100]
+print(beforeBecause, lastBecause)
+
+##26. Find the position of the first occurrence of the word 'because' in the following sentence: 
+# 'You cannot end a sentence with because because because is a conjunction'
+
+print(sentence.index('because'))
+
+##27.Slice out the phrase 'because because because' in the following sentence: 
+# 'You cannot end a sentence with because because because is a conjunction'
+
+
+
+##28. Does '\'Coding For All' start with a substring _Coding_?
+
+
+
+##29. Does 'Coding For All' end with a substring _coding_?
+
+
+
+##30.'&nbsp;&nbsp; Coding For All &nbsp;&nbsp;&nbsp; &nbsp;' &nbsp;
+# , remove the left and right trailing spaces in the given string.
+
+
+
+##31. Which one of the following variables return True when we use the method isidentifier():
+#   - 30DaysOfPython
+#   - thirty_days_of_python
+
+
+
+##32 The following list contains the names of some of python libraries: 
+# ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+
+
+
+##33. Use the new line escape sequence to separate the following sentences.
+#   ```py
+#   I am enjoying this challenge.
+#   I just wonder what is next.
+#   ```
+
+
+
+##34. Use a tab escape sequence to write the following lines.
+#   ```py
+#   Name      Age     Country   City
+#   Asabeneh  250     Finland   Helsinki
+#   ```
+
+
+
+##35.  Use the string formatting method to display the following:
+#```sh
+#radius = 10
+#area = 3.14 * radius ** 2
+#The area of a circle with radius 10 is 314 meters square.
+#```
+
+
+
+##36. Make the following using string formatting methods:
+#```sh
+#8 + 6 = 14
+#8 - 6 = 2
+#8 * 6 = 48
+#8 / 6 = 1.33
+#8 % 6 = 2
+#8 // 6 = 1
+#8 ** 6 = 262144
+#```
+
 
 
