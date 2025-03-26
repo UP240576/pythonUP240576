587cbb276e52c88ca3aeea33b25007a9555ff51f
Leonardolap <up250576@alumnos.upa.edu.mx>
2025-03-26 10:08:14 -0600

    Avance dia 11

---
+++ Dia 11 Functions/Ejercicios.py
@@ -0,0 +1,258 @@
+##1. Declare a function _add_two_numbers_. 
+# It takes two parameters and it returns a sum.
+
+print('''Ejercicio 1 nivel 1:
+      Declara una funcion que se entreguen dos numeros y que devuela una suma
+''')
+
+def addTwoNumbers():
+    frstNum = float(input('ingresa un numero'))
+    scndNum = 8
+    sum = frstNum + scndNum
+    return 'La suma es', sum
+print(addTwoNumbers())
+
+##2. Area of a circle is calculated as follows: area = π x r x r. 
+# Write a function that calculates _area_of_circle_.
+
+print('''Ejercicio 2 nivel 1:
+    Escribe una funcion que calcule el area de un circulo
+''')
+
+def areaDeUnCirculo ():
+    r = 10
+    pi = 3.1416
+    area = pi * r * r
+    return 'El area de un circulo es: ', area
+print(areaDeUnCirculo())
+
+##3. Write a function called add_all_nums which takes arbitrary number of arguments and sums all the arguments. 
+# Check if all the list items are number types. If not do give a reasonable feedback.
+
+print('''Ejercicio 3 nivel 1:
+      Escribe una funcion qque se llame add_all_nums que tome numeros 
+''')
+
+
+
+##4. Temperature in °C can be converted to °F using this formula: °F = (°C x 9/5) + 32. 
+# Write a function which converts °C to °F, _convert_celsius_to-fahrenheit_.
+
+print('''Ejercicio 4 nivel 1:
+      
+''')
+
+
+
+##5. Write a function called check-season, it takes a month parameter and returns the season:
+#  Autumn, Winter, Spring or Summer.
+
+print('''Ejercicio 5 nivel 1:
+      
+''')
+
+
+
+##6. Write a function called calculate_slope which return the slope of a linear equation
+
+print('''Ejercicio 6 nivel 1:
+      
+''')
+
+
+
+##7. Quadratic equation is calculated as follows: ax² + bx + c = 0. 
+# Write a function which calculates solution set of a quadratic equation, _solve_quadratic_eqn_.
+
+print('''Ejercicio 7 nivel 1:
+      
+''')
+
+
+
+##8. Declare a function named print_list. 
+# It takes a list as a parameter and it prints out each element of the list.
+
+print('''Ejercicio 8 nivel 1:
+      
+''')
+
+
+
+##9. Declare a function named reverse_list. 
+# It takes an array as a parameter and it returns the reverse of the array (use loops).
+
+#```py
+#print(reverse_list([1, 2, 3, 4, 5]))
+# [5, 4, 3, 2, 1]
+#print(reverse_list1(["A", "B", "C"]))
+# ["C", "B", "A"]
+#```
+
+print('''Ejercicio 9 nivel 1:
+      
+''')
+
+
+
+##10. Declare a function named capitalize_list_items. 
+# It takes a list as a parameter and it returns a capitalized list of items
+
+print('''Ejercicio 10 nivel 1:
+       
+''')
+
+
+
+##11. Declare a function named add_item. It takes a list and an item parameters. 
+# It returns a list with the item added at the end.
+
+#```py
+#food_staff = ['Potato', 'Tomato', 'Mango', 'Milk']
+#print(add_item(food_staff, 'Meat'))     # ['Potato', 'Tomato', 'Mango', 'Milk','Meat']
+#numbers = [2, 3, 7, 9]
+#print(add_item(numbers, 5))      [2, 3, 7, 9, 5]
+#```
+
+print('''Ejercicio 11 nivel 1:
+      
+''')
+
+
+
+##12. Declare a function named remove_item. It takes a list and an item parameters. 
+# It returns a list with the item removed from it.
+
+#```py
+#food_staff = ['Potato', 'Tomato', 'Mango', 'Milk']
+#print(remove_item(food_staff, 'Mango'))  # ['Potato', 'Tomato', 'Milk'];
+#numbers = [2, 3, 7, 9]
+#print(remove_item(numbers, 3))  # [2, 7, 9]
+#```
+
+print('''Ejercicio 12 nivel 1:
+      
+''')
+
+
+
+##13. Declare a function named sum_of_numbers. 
+# It takes a number parameter and it adds all the numbers in that range.
+
+#```py
+#print(sum_of_numbers(5))  # 15
+#print(sum_of_numbers(10)) # 55
+#print(sum_of_numbers(100)) # 5050
+#```
+
+print('''Ejercicio 13 nivel 1:
+      
+''')
+
+
+
+##14. Declare a function named sum_of_odds. 
+# It takes a number parameter and it adds all the odd numbers in that range.
+
+print('''Ejercicio 14 nivel 1:
+      
+''')
+
+##15. Declare a function named sum_of_even. 
+#It takes a number parameter and it adds all the even numbers in that - range.
+
+print('''Ejercicio 15 nivel 1:
+      
+''')
+
+### Exercises: Level 2
+
+##1. Declare a function named evens_and_odds . 
+# It takes a positive integer as parameter and it counts number of evens and odds in the number.
+
+#```py
+#    print(evens_and_odds(100))
+    # The number of odds are 50.
+    # The number of evens are 51.
+#```
+
+print('''Ejercicio 1 nivel 2:
+      
+''')
+
+
+
+##2. Call your function factorial, 
+# it takes a whole number as a parameter and it return a factorial of the number
+
+print('''Ejercicio 2 nivel 2:
+      
+''')
+
+
+
+##3. Call your function _is_empty_, 
+# it takes a parameter and it checks if it is empty or not
+
+print('''Ejercicio 3 nivel 2:
+      
+''')
+
+
+
+##4. Write different functions which take lists. 
+# They should calculate_mean, calculate_median, calculate_mode, calculate_range, 
+# calculate_variance, calculate_std (standard deviation).
+
+print('''Ejercicio 4 nivel 2:
+      
+''')
+
+
+
+### Exercises: Level 3
+
+##1. Write a function called is_prime, which checks if a number is prime.
+
+print('''Ejercicio 1 nivel 3:
+      
+''')
+
+
+
+##2. Write a functions which checks if all items are unique in the list.
+
+print('''Ejercicio 2 nivel 3:
+      
+''')
+
+
+
+##3. Write a function which checks if all the items of the list are of the same data type.
+
+print('''Ejercicio 3 nivel 3:
+      
+''')
+
+
+
+##4. Write a function which check if provided variable is a valid python variable
+
+print('''Ejercicio 4 nivel 3:
+      
+''')
+
+
+
+##5. Go to the data folder and access the countries-data.py file.
+
+#- Create a function called the most_spoken_languages in the world. 
+# It should return 10 or 20 most spoken languages in the world in descending order
+#- Create a function called the most_populated_countries. 
+# It should return 10 or 20 most populated countries in descending order.
+
+print('''Ejercicio 5 nivel 3:
+      
+''')
+
+
