52678565992fd5107617920f984db04025c1b8fb
UP240576 <UP240576@alumnos.upa.edu.mx>
2025-04-03 19:48:42 -0600

    Dia 12 archivos

---
+++ Dia 12 Modules/Ejercicios lvl 3.py
@@ -0,0 +1,10 @@
+### Exercises: Level 3
+
+##1. Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+
+
+
+##2. Write a function which returns an array of seven random numbers in a range of 0-9.
+# All the numbers must be unique.
+
+
