cc5c8627350050e0b99961d0a663ab8a3671d5e9
UP240576 <UP240576@alumnos.upa.edu.mx>
2025-04-05 00:22:11 -0600

    Dia 15 finalizado

---
+++ Dia 15 Type Errors/Ejercicios.py
@@ -0,0 +1,33 @@
+## 💻 Exercises: Day 15
+
+##1. Open you python interactive shell and try all the examples covered in this section.
+
+print(''' Ejemplo 1: sintaxis
+      no cerrar el parentesis y/o no agregar parentesis
+''')
+
+print(''' Ejemplo 2: str
+      olvidar agregar comillas "" ''
+''')
+
+print(''' Ejemplo 3: index
+      numeros=[1,2,3,4,5,]
+      print(numeros[20])
+''')
+
+print(''' Ejemplo 4: modulo
+      importar 
+''')
+
+print(''' Ejemplo 5: attribute
+      import.range
+      range.ends
+''')
+
+print(''' Ejemplo 6: key
+      imprimir un elemento que no exista en la lista
+''')
+
+print(''' Ejempl 7: valor
+      Es cuando le damos un valor como uno numerico a uno que es en realidad una str
+''')
